ab9793dbf5ffa51d9704908f3b2d803c9de71d06
myrtill3 <m.yreil@hotmail.ca>
2026-07-28 16:08:12 -0400

    Update script.js

--- script.js
+++ script.js
@@ -51,7 +51,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
     // Add "All" button first
-    filterContainer.innerHTML = `
+    filterContainer.prepend = `
         <button data-filter="*">
             All
         </button>
