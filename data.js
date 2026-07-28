fd44076d1b3f62a04f77e63237f006b2b6617955
unravelgraph <80479658+unravelgraph@users.noreply.github.com>
2026-07-28 19:32:49 +0000

    Update generated gallery data

---
+++ data.js
@@ -0,0 +1,72 @@
+const PEOPLE = [
+    {
+        "id": "bae-ganghee",
+        "folder": "bae ganghee",
+        "folders": {
+            "avatars": [
+                "ganghee1.gif"
+            ]
+        },
+        "name": "Bae Ganghee",
+        "dob": "1997",
+        "ethnicity": [
+            "Korean"
+        ],
+        "lastAdded": "2026-07-28"
+    },
+    {
+        "id": "heyu",
+        "folder": "heyu",
+        "folders": {
+            "avatars": [
+                "heyu1.gif"
+            ],
+            "gifs": [
+                "heyugif1.gif"
+            ]
+        },
+        "name": "He Yu",
+        "dob": "1996",
+        "ethnicity": [
+            "Chinese"
+        ],
+        "lastAdded": "2026-07-28"
+    },
+    {
+        "id": "zhang-linghe",
+        "folder": "zhang linghe",
+        "folders": {
+            "avatars": [
+                "zlh1.gif",
+                "zlh2.gif"
+            ]
+        },
+        "name": "Zhang Linghe",
+        "dob": "1997",
+        "ethnicity": [
+            "Chinese"
+        ],
+        "lastAdded": "2026-07-28"
+    },
+    {
+        "id": "zhou-keyu",
+        "folder": "zhou keyu",
+        "folders": {
+            "avatars": [
+                "keyu1.gif"
+            ],
+            "gifs": [
+                "keyu2.gif",
+                "keyu3.gif",
+                "keyu4.gif",
+                "keyu5.gif"
+            ]
+        },
+        "name": "Daniel Zhou Keyu",
+        "dob": "2002",
+        "ethnicity": [
+            "Chinese"
+        ],
+        "lastAdded": "2026-07-28"
+    }
+];
