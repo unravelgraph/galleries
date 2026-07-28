0fb7c09d7f2ed25bcbab150f0e257530547beeec
unravelgraph <80479658+unravelgraph@users.noreply.github.com>
2026-07-28 18:30:41 +0000

    Update generated gallery data

---
+++ data.js
@@ -0,0 +1,39 @@
+const PEOPLE = [
+    {
+        "id": "heyu",
+        "folder": "heyu",
+        "folders": {
+            "avatars": [
+                "heyu1.gif"
+            ],
+            "gifs": [
+                "heyugif1.gif"
+            ]
+        },
+        "name": "He Yu",
+        "dob": "1996",
+        "ethnicity": [
+            "Chinese"
+        ]
+    },
+    {
+        "id": "zhou-keyu",
+        "folder": "zhou keyu",
+        "folders": {
+            "avatars": [
+                "keyu1.gif"
+            ],
+            "gifs": [
+                "keyu2.gif",
+                "keyu3.gif",
+                "keyu4.gif",
+                "keyu5.gif"
+            ]
+        },
+        "name": "Daniel Zhou Keyu",
+        "dob": "2002",
+        "ethnicity": [
+            "Chinese"
+        ]
+    }
+];
