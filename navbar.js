0f1cb6f26c4ec3c90bacad3ac2f5006ec5caf34e
myrtill3 <m.yreil@hotmail.ca>
2026-07-28 17:46:37 -0400

    Update navbar.js

--- navbar.js
+++ navbar.js
@@ -1,9 +1,6 @@
 const navbar = document.querySelector("#navbar");
 
-    if (!navbar) return;
-
-
-    fetch("navbar.html")
+fetch("navbar.html")
         .then(response => response.text())
         .then(html => {
 
